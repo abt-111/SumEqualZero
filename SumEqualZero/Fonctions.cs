8ec464f69a60556499dc7529c6532a7b54b9f8ee
abt93 <anthony.temmel@gmail.com>
2024-06-13 11:37:25 +0200

    Déplacement du code dans des fonctions

---
+++ SumEqualZero/Fonctions.cs
@@ -0,0 +1,54 @@
+﻿public static class Fonctions
+{
+    public static void Generation(int[] entiers)
+    {
+        int n = entiers.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i < (n / 2))
+            {
+                GenerationEntierUnique(entiers, i, -100, 0);
+            }
+            else if (i == (n - 1))
+            {
+                entiers[i] = -entiers.Sum();
+            }
+            else
+            {
+                GenerationEntierUnique(entiers, i, 0, 100);
+            }
+        }
+    }
+
+    public static void GenerationEntierUnique(int[] entiers, int i, int min, int max)
+    {
+        Random random = new Random();
+        int entierTemporaire;
+        // On vérifie que l'entier n'est pas déjà présent dans le tableau
+        do
+        {
+            entierTemporaire = Convert.ToInt32(random.NextInt64(min, max));
+        }
+        while (entiers.Contains(entierTemporaire));
+
+        entiers[i] = entierTemporaire;
+    }
+
+    public static void Affichage(int[] entiers)
+    {
+        int n = entiers.Length;
+        string chaine = "[";
+
+        for (int i = 0; i < n; i++)
+        {
+            chaine += entiers[i];
+            if (i < (n - 1))
+            {
+                chaine += ",";
+            }
+        }
+        chaine += "]";
+        Console.WriteLine(chaine);
+    }
+}
--- SumEqualZero/Program.cs
+++ SumEqualZero/Program.cs
@@ -2,8 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Random random = new Random();
-        int n, entierTemporaire;
+        int n;
         int[] entiers;
 
         Console.Write("Entrée : n = ");
@@ -11,49 +10,13 @@
 
         entiers = new int[n];
 
-        for (int i = 0; i < n; i++)
-        {
-            if(i < (n / 2))
-            {
-                // On vérifie que l'entier n'est pas déjà présent dans le tableau
-                do
-                {
-                    entierTemporaire = Convert.ToInt32(random.NextInt64(-100, 0));
-                }
-                while (entiers.Contains(entierTemporaire));
-
-                entiers[i] = entierTemporaire;
-            }
-            else if(i == (n - 1))
-            {
-                entiers[i] = -entiers.Sum();
-            }
-            else
-            {
-                // On vérifie que l'entier n'est pas déjà présent dans le tableau
-                do
-                {
-                    entierTemporaire = Convert.ToInt32(random.NextInt64(0, 100));
-                }
-                while (entiers.Contains(entierTemporaire));
-
-                entiers[i] = entierTemporaire;
-            }
-        }
+        // Génération
+        Fonctions.Generation(entiers);
 
         // Affichage sous la forme [-7,-1,1,3,4]
-        string chaine = "[";
-        for (int i = 0; i < n; i++)
-        {
-            chaine += entiers[i];
-            if(i < (n - 1))
-            {
-                chaine += ",";
-            }
-        }
-        chaine += "]";
-        Console.WriteLine(chaine);
+        Fonctions.Affichage(entiers);
 
+        // Vérification de la valeure de somme
         // Console.WriteLine(entiers.Sum());
     }
 }
