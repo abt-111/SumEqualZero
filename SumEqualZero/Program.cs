e8292b1b07edea9f6f45906f80137112f0d6bbbf
abt93 <anthony.temmel@gmail.com>
2024-06-13 10:34:55 +0200

    Ajout de la génération des entiers

--- SumEqualZero/Program.cs
+++ SumEqualZero/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int n, somme, entierTemporaire;
+        int[] entiers;
+
+        Console.Write("Entrez un entier : ");
+        // Que des n pairs pour le moment pour le moment
+        n = Convert.ToInt32(Console.ReadLine());
+
+        entiers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if(i < (n / 2))
+            {
+                entiers[i] = Convert.ToInt32(random.NextInt64(-100, 0));
+            }
+            else if(i == (n - 1))
+            {
+                entiers[i] = -entiers.Sum();
+            }
+            else
+            {
+                entiers[i] = Convert.ToInt32(random.NextInt64(0, 100));
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine(entiers[i]);
+        }
+
+        Console.WriteLine(entiers.Sum());
+    }
+}
